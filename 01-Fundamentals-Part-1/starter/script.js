1b5faa058b8fb93a0845667ca8fed32bbc4a56a2
Siyuan Du(Steven) <major349435389@gmail.com>
2023-12-15 21:12:50 -0400

    Lecture 1 -20

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,95 @@
+// let js = 'amazing';
+// // console.log(40 + 8 + 23 - 10);
+
+// console.log("James");
+// console.log("23");
+
+// let firstname = "Jonas";
+// console.log(firstname);
+// console.log(firstname);
+// console.log(firstname);
+
+// //
+// let jonas_matilda = "JM";
+// let sfunction = 27;
+
+// let person = 'jones';
+// let PI = 3.1415;
+
+// let myFirstJob = "Programmer";
+// let myCurrentJob = "Teacher";
+
+// let job1 = 'programmer';
+// let job2 = 'teacher';
+
+// console.log(myFirstJob);
+
+
+// let javascriptIsFun = true;
+// console.log(javascriptIsFun);
+
+// // console.log(typeof true);
+// console.log(typeof javascriptIsFun);
+// // console.log(typeof 23);
+// // console.log(typeof 'Jones');
+
+// javascriptIsFun = 'YES!';
+// console.log(typeof javascriptIsFun);
+
+// let year;
+// console.log(year);
+// console.log(typeof year);
+
+// year = 1991;
+// console.log(typeof year);
+
+// console.log(typeof null);
+
+// let age = 30;//var
+// age = 31;
+
+//(Uncaught TypeError: Assignment to constant variable.)
+const birthYear = 1991; //create the variable that we not reassign, the variable could not be muteted type error, 
+// birthYear = 1991;
+
+//Missing initializer in const declaration
+// const job; //when we use const, we need basically an initial value
+//clean code: use const by default and let only sure that the variable need to changes
+
+// var job = "programming";
+// job = 'teacher';
+
+// lastName = "Schemedmean";
+// console.log(lastName);
+
+//Basic Operator(arithmetic operations)
+// const now = 2037;
+// const ageJones = now - 1991;
+// const ageSarach = now - 2020;
+// console.log(ageJones, ageSarach);
+
+// console.log(ageJones * 2, ageJones / 10, 2 ** 3);
+// // 2**3 means 2 the power of 3 = 2 * 2 * 2
+
+// const firstName = 'Jones';
+// const lastName = 'Schmedtmann';
+// console.log(firstName + " " + lastName);
+
+// //assignment operator
+// let x = 10 + 5; //15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
+
+// //Comparison operators
+// console.log(ageJones > ageSarach); // ageJones 46, age Sarach 19 >, <, >=, <=
+// console.log(ageSarach >= 18);
+
+// const isFullAge = ageSarach >= 18;
+
+// console.log(now - 1991 > now - 2018);
+
+
+//
