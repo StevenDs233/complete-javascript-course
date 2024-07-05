6688aaed5274beea3e9b0ea8451ae603b1a1dff5
Du Siyuan <dz619987@dal.ca>
2024-07-05 16:55:02 -0300

    jh

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -166,23 +166,23 @@ GOOD LUCK 😀
 // lines`);
 
 /*if else statement*/
-const age = 15;
+// const age = 15;
 
-if (age >= 18) {
-    console.log("Sarah can start driving license🚗")
-} else {
-    const yearsLeft = 18 - age;
-    console.log(`Sarah is too young, Wait another ${yearsLeft} years:)`)
-}
+// if (age >= 18) {
+//     console.log("Sarah can start driving license🚗")
+// } else {
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young, Wait another ${yearsLeft} years:)`)
+// }
 
-const birthYear = 1998;
-let century;
-if (birthYear <= 2000) {
-    century = 20;
-} else {
-    century = 21;
-}
-console.log(century);
+// const birthYear = 1998;
+// let century;
+// if (birthYear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century);
 
 
 
@@ -197,7 +197,7 @@ Use the BMI example from Challenge #1, and the code you already wrote, and impro
 HINT: Use an if/else statement 😉
 
 GOOD LUCK 😀
-
+*/
 
 // const markWeight = 78;
 // const markHeight = 1.69;
@@ -219,6 +219,177 @@ GOOD LUCK 😀
 // } else {
 //     console.log(`John's BMI (${JohnBMI}) is higher than Mark's (${markBMI})!`);
 // }
-*/
+
 
 //Type Conversion and Coercion
+// const inputYear = '1991';
+// console.log(Number(inputYear),inputYear);
+// console.log(Number(inputYear) + 18);
+
+// console.log(Number('Jonas'));
+// console.log(typeof NaN);
+
+// console.log(String(23),23);
+
+// //type coercion
+// console.log('i am ' + 23 + ' years old');
+// console.log('23' -'10' - 3);
+// console.log('23' * '2');
+// console.log('23' / '2');
+
+// let n = '1' + 1 //to the string
+// n = n - 1;
+// console.log(n);
+
+//Truthuy abd Faisy values
+//5 falsy value: 0, ''. undefined, null, NaN
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('Jonas'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// const money = 0;
+// if(money){
+//    console.log("Don't spend it all;)");
+// } else{
+// console.log("You should get a job");
+// }
+
+/*Equality operator == or ===*/
+// const age = '18';
+// if(age === 18){
+//     console.log('You Just became an adult:D(strict)');
+// }
+
+// if(age == 18){
+//     console.log('You Just became an adult:D(loose)');
+// }
+
+// const favorite = Number(prompt("Waht is your favourite number?"));
+// console.log(favorite);
+// console.log(typeof favorite);
+
+// if(favorite === 23){ // '23' == 23
+//    console.log('Cool! 23 is an amazing number!');
+// }else if(favorite === 7){
+//     console.log('7 is a cool number!');
+// }else if(favorite === 9){
+//     console.log('9 is a cool number!');
+// }else{
+//     console.log('Number is not 23 or 7 or 9');
+// }
+
+// if(favorite !== 23) console.log('Why not 23!');
+
+//Boolean logic
+// const hasDriversLicense = true;
+// const hasGoodVision = false;
+
+// console.log(hasDriversLicense && hasGoodVision);
+// console.log(hasDriversLicense || hasGoodVision);
+// console.log(!hasDriversLicense);
+
+// if(hasDriversLicense && hasGoodVision){
+//     console.log('Sarah is able to drive!');
+// }else{
+//     console.log('Someone else should drive...');
+// }
+
+// const isTired = true;//c
+// console.log(hasDriversLicense && hasGoodVision && isTired);
+
+// Coding Challenge #3
+
+/*
+There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins the a trophy!
+
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, and print it to the console. Don't forget that there can be a draw, so test for that as well (draw means they have the same average score).
+
+3. BONUS 1: Include a requirement for a minimum score of 100. With this rule, a team only wins if it has a higher score than the other team, and the same time a score of at least 100 points. HINT: Use a logical operator to test for minimum score, as well as multiple else-if blocks 😉
+4. BONUS 2: Minimum score also applies to a draw! So a draw only happens when both teams have the same score and both have a score greater or equal 100 points. Otherwise, no team wins the trophy.
+
+TEST DATA: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+TEST DATA BONUS 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+TEST DATA BONUS 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+
+GOOD LUCK 😀
+*/
+
+// const scoreDolphins = (96 + 108 + 89) / 3;
+// const scoreKoalas = (88 + 91 + 110) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas) {
+//   console.log('Both win the trophy!');
+// }
+
+// BONUS 1
+// const scoreDolphins = (97 + 112 + 80) / 3;
+// const scoreKoalas = (109 + 95 + 50) / 3;
+// console.log(scoreDolphins, scoreKoalas);
+
+// if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
+//   console.log('Dolphins win the trophy 🏆');
+// } else if (scoreKoalas > scoreDolphins && scoreKoalas >= 100) {
+//   console.log('Koalas win the trophy 🏆');
+// } else if (scoreDolphins === scoreKoalas && scoreDolphins >= 100 && scoreKoalas >= 100) {
+//   console.log('Both win the trophy!');
+// } else {
+//   console.log('No one wins the trophy 😭');
+// }
+
+/*Swtich Statement
+
+const day = 'friday';
+
+switch (day) {
+  case 'monday': // day === 'monday'
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+    break;
+  case 'tuesday':
+    console.log('Prepare theory videos');
+    break;
+  case 'wednesday':
+  case 'thursday':
+    console.log('Write code examples');
+    break;
+  case 'friday':
+    console.log('Record videos');
+    break;
+  case 'saturday':
+  case 'sunday':
+    console.log('Enjoy the weekend :D');
+    break;
+  default:
+    console.log('Not a valid day!');
+}
+
+if (day === 'monday') {
+    console.log('Plan course structure');
+    console.log('Go to coding meetup');
+  } else if (day === 'tuesday') {
+    console.log('Prepare theory videos');
+  } else if (day === 'wednesday' || day === 'thursday') {
+    console.log('Write code examples');
+  } else if (day === 'friday') {
+    console.log('Record videos');
+  } else if (day === 'saturday' || day === 'sunday') {
+    console.log('Enjoy the weekend :D');
+  } else {
+    console.log('Not a valid day!');
+  }
+  */
+
+  
+
+
+
+
+
